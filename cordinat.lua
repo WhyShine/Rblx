fca2b6994b00a6a9353f6314475eed921fca9fd8
WhyShine <156557561+WhyShine@users.noreply.github.com>
2024-07-02 16:14:17 +0700

    Update cordinat.lua

--- cordinat.lua
+++ cordinat.lua
@@ -15,7 +15,6 @@ local LogsTab = Window:MakeTab({
 local lastLogTime = 0
 local logCooldown = 2 -- cooldown in seconds
 local coordinateLogs = {}
-local interactionLogs = {}
 
 -- Function to get current position with 2 decimal precision
 local function getCurrentPosition()
@@ -35,27 +34,9 @@ local function logCoordinates()
     end
 end
 
--- Function to log interactions
-local function logInteraction(interaction)
-    local currentTime = tick()
-    if currentTime - lastLogTime >= logCooldown then
-        table.insert(interactionLogs, interaction)
-        lastLogTime = currentTime
-    end
-end
-
 -- Box to display coordinates
 LogsTab:AddLabel("Coordinates:")
 local coordinateLabel = LogsTab:AddLabel("")
-
--- Box to display interactions
-LogsTab:AddLabel("Interactions:")
-local interactionBox = LogsTab:AddTextbox({
-    Name = "Interaction Log",
-    Default = "",
-    TextDisappear = false,
-    Callback = function() end
-})
 
 -- Update the coordinate label continuously
 spawn(function()
@@ -65,42 +46,6 @@ spawn(function()
         wait(0.1) -- update every 0.1 second
     end
 end)
-
--- Function to update interaction box
-local function updateInteractionBox()
-    local logText = table.concat(interactionLogs, "\n")
-    interactionBox:Set(logText)
-end
-
--- Example interaction events
-game.Players.LocalPlayer.CharacterAdded:Connect(function(character)
-    logInteraction("Character spawned: " .. tostring(character))
-    updateInteractionBox()
-end)
-
-game.Players.LocalPlayer.Chatted:Connect(function(message)
-    logInteraction("User said: " .. message)
-    updateInteractionBox()
-end)
-
--- Log interactions with NPCs
-for _, npc in pairs(game.Workspace.NPCs:GetChildren()) do
-    npc.Touched:Connect(function(hit)
-        if hit.Parent == game.Players.LocalPlayer.Character then
-            logInteraction("Interacted with NPC: " .. npc.Name .. " - Action: Touch")
-            updateInteractionBox()
-        end
-    end)
-    
-    if npc:FindFirstChild("Attack") then
-        npc.Attack.OnServerEvent:Connect(function(player)
-            if player == game.Players.LocalPlayer then
-                logInteraction("Attacked by NPC: " .. npc.Name)
-                updateInteractionBox()
-            end
-        end)
-    end
-end
 
 -- Initiate Orion library
 OrionLib:Init()
